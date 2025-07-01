c1100717032ce6a1503943f208969e0dc16a4251
su8y <su8y.dev@gmail.com>
2025-07-01 14:56:34 +0900

    init spring preprocessor

---
+++ preprocessor/src/main/java/com/example/preprocessor/PreprocessorApplication.java
@@ -0,0 +1,13 @@
+package com.example.preprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreprocessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreprocessorApplication.class, args);
+	}
+
+}
---
+++ preprocessor/src/test/java/com/example/preprocessor/PreprocessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.preprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreprocessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
